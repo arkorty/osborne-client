a6927a6d8434c41dfba6e7bcd8dee50ea9c14c12
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-11-13 18:24:46 +0530

    style: major UI overhaul

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,31 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
-import { Button } from "../components/ui/button";
-import { Input } from "../components/ui/input";
-import { Card, CardContent } from "../components/ui/card";
-import { BackgroundBeams } from "../components/ui/background-beams";
+import { Button } from "@/components/ui/button";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
+import { Card, CardContent } from "@/components/ui/card";
+import Image from "next/image";
 
 const Home = () => {
   const router = useRouter();
   const [newRoomCode, setNewRoomCode] = useState("");
 
-  const joinRoom = () => {
-    if (newRoomCode) {
-      router.push(`/room?code=${newRoomCode.toUpperCase()}`);
+  useEffect(() => {
+    const joinRoom = () => {
+      if (newRoomCode) {
+        router.push(`/room?code=${newRoomCode.toUpperCase()}`);
+      }
+    };
+
+    if (newRoomCode.length === 6) {
+      joinRoom();
     }
-  };
+  }, [newRoomCode, router]);
 
   const createNewRoom = () => {
     const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
@@ -28,54 +38,43 @@ const Home = () => {
 
   return (
     <div className="relative min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900">
-      <BackgroundBeams className="absolute inset-0 w-full h-full z-0" />
       <Card className="relative z-10 max-w-md backdrop-blur-sm shadow-lg bg-white/0 bg-opacity-0 dark:bg-gray-800/70 border border-gray-300 dark:border-gray-700 rounded-2xl p-6 flex flex-col items-center">
-        <Card className="relative z-10 w-[172px] h-[172px] backdrop-blur-md shadow-lg bg-white/70 dark:bg-gray-800/70 border border-gray-300 dark:border-gray-700 rounded-xl flex flex-col items-center justify-center">
-          <div className="flex flex-col items-center">
-            <svg
-              className="w-16 h-16 mb-4"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            >
-              <rect x="3" y="2" width="18" height="20" rx="2" ry="2" />
-              <rect x="10" y="12" width="4" height="8" />
-              <line x1="10" y1="2" x2="10" y2="12" />
-            </svg>
-            <span className="text-4xl font-bold text-center text-gray-800 dark:text-white">
-              Room
-            </span>
+        <div className="flex flex-col items-center">
+          <div className={`rounded-2xl m-8 bg-black`}>
+            <Image
+              src="/logo.png"
+              alt="Room Logo"
+              width={128}
+              height={128}
+              className={`m-2`}
+            ></Image>
           </div>
-        </Card>
-        <CardContent className="mt-4 flex flex-col items-center space-y-4">
+        </div>
+        <CardContent className="flex flex-col items-center space-y-4">
+          <InputOTP
+            value={newRoomCode}
+            onChange={(value) => setNewRoomCode(value.toUpperCase())}
+            maxLength={6}
+            pattern="[A-Z0-9]*"
+          >
+            <InputOTPGroup>
+              {[...Array(6)].map((_, index) => (
+                <InputOTPSlot
+                  key={index}
+                  index={index}
+                  className="text-primary bg-white dark:text-secondary dark:caret-white"
+                />
+              ))}
+            </InputOTPGroup>
+          </InputOTP>
+          <span className="text-lg text-primary/70">or</span>
           <Button
             onClick={createNewRoom}
             variant="default"
-            className="w-full bg-blue-600 text-white py-3 text-lg font-semibold rounded-lg hover:bg-blue-700"
+            className="w-min bg-blue-600 text-white text-lg font-semibold rounded-lg hover:bg-blue-700"
           >
-            Create New Room
+            Create Room
           </Button>
-          <div className="flex items-center gap-3">
-            <Input
-              type="text"
-              value={newRoomCode}
-              onChange={(e) => setNewRoomCode(e.target.value.toUpperCase())}
-              placeholder="Enter Room Code"
-              maxLength={6}
-              className="flex-1 border-gray-300 dark:border-gray-600 rounded-lg"
-            />
-            <Button
-              onClick={joinRoom}
-              variant="default"
-              className="bg-green-500 text-white py-3 px-5 text-lg font-semibold rounded-lg hover:bg-green-600"
-            >
-              Join
-            </Button>
-          </div>
         </CardContent>
       </Card>
     </div>
--- app/room/page.tsx
+++ app/room/page.tsx
@@ -2,12 +2,18 @@
 
 import { useEffect, useState, useCallback, useRef, Suspense } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Textarea } from "@/components/ui/textarea";
+import { Badge } from "@/components/ui/badge";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
+import { Link2, LogOut } from "lucide-react";
+import { Skeleton } from "@/components/ui/skeleton";
 import debounce from "lodash/debounce";
-import { Button } from "../../components/ui/button";
-import { Card, CardContent, CardHeader } from "../../components/ui/card";
-import { BackgroundBeams } from "../../components/ui/background-beams";
-import { Textarea } from "../../components/ui/textarea";
-import { Badge } from "../../components/ui/badge";
 import dotenv from "dotenv";
 
 dotenv.config();
@@ -54,8 +60,8 @@ const Room = () => {
     setIsClient(true);
   }, []);
 
-  const debouncedSend = useCallback(
-    debounce((ws: WebSocket, content: string, code: string) => {
+  const debouncedSend = debounce(
+    (ws: WebSocket, content: string, code: string) => {
       if (ws.readyState === WebSocket.OPEN) {
         const message: TextUpdate = {
           type: "text-update",
@@ -64,8 +70,8 @@ const Room = () => {
         };
         ws.send(JSON.stringify(message));
       }
-    }, 100),
-    [],
+    },
+    100
   );
 
   const connectSocket = useCallback(() => {
@@ -159,22 +165,39 @@ const Room = () => {
 
   return (
     <div className="relative min-h-screen">
-      <BackgroundBeams className="absolute top-0 left-0 w-full h-full z-0" />
       <div className="flex flex-col items-center p-4 relative z-10">
-        <Card className="w-full max-w-4xl bg-inherit backdrop-blur-sm bg-opacity-0">
+        <Card className="w-full max-w-6xl bg-inherit backdrop-blur-sm bg-opacity-0">
           <CardHeader className="flex flex-row items-center justify-between">
-            <Button
-              className="text-lg bg-violet-400 hover:bg-violet-500 font-semibold"
-              onClick={() => {
-                navigator.clipboard.writeText(roomCode);
-                alert("Room code copied to clipboard!");
-              }}
-            >
-              {roomCode}
-            </Button>
-            <Badge variant={status === "Connected" ? "success" : "destructive"}>
-              {status}
-            </Badge>
+            <HoverCard>
+              <HoverCardTrigger>
+                <Button
+                  className="text-lg bg-violet-400 hover:bg-violet-500 font-semibold"
+                  onClick={() => {
+                    navigator.clipboard.writeText(roomCode);
+                    alert("Room code copied to clipboard!");
+                  }}
+                >
+                  {roomCode}
+                </Button>
+              </HoverCardTrigger>
+              <HoverCardContent className="py-1 px-2 w-auto bg-violet-200 text-xs">
+                copy room code
+              </HoverCardContent>
+            </HoverCard>
+            <HoverCard>
+              <HoverCardTrigger>
+                <Badge
+                  variant={status === "Connected" ? "success" : "destructive"}
+                >
+                  {status}
+                </Badge>
+              </HoverCardTrigger>
+              <HoverCardContent className="py-1 px-2 w-auto bg-green-200 text-xs">
+                {status === "Connected"
+                  ? "connected to the server"
+                  : "not connected to the server"}
+              </HoverCardContent>
+            </HoverCard>
           </CardHeader>
           <CardContent>
             {error && status !== "Connected" && (
@@ -185,28 +208,74 @@ const Room = () => {
             <Textarea
               value={content}
               onChange={(e) => handleContentChange(e.target.value)}
-              className="w-full min-h-[80vh] p-4 rounded-lg bg-neutral-100 border border-gray-800 focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
-              placeholder="Start typing here..."
+              className="w-full min-h-[85vh] p-4 rounded-lg bg-neutral-100 border border-gray-300"
+              placeholder="What's on your mind?"
             />
             <div className="mt-4 flex justify-end items-center">
               <div className="flex gap-2">
-                <Button
-                  variant="default"
-                  className="font-semibold text-white bg-blue-400 hover:bg-blue-500"
-                  onClick={() => {
-                    navigator.clipboard.writeText(window.location.href);
-                    alert("Room link copied to clipboard!");
-                  }}
-                >
-                  Copy Link
-                </Button>
-                <Button
-                  className="font-semibold bg-red-500 hover:bg-red-600"
-                  variant="destructive"
-                  onClick={() => router.push("/")}
-                >
-                  Leave
-                </Button>
+                <HoverCard>
+                  <HoverCardTrigger>
+                    <Button
+                      variant="default"
+                      className="w-10 h-10 text-white bg-blue-400 hover:bg-blue-500"
+                      onClick={() => {
+                        navigator.clipboard.writeText(window.location.href);
+                        alert("Room link copied to clipboard!");
+                      }}
+                    >
+                      <Link2></Link2>
+                    </Button>
+                  </HoverCardTrigger>
+                  <HoverCardContent
+                    className="py-1 px-2 w-auto bg-blue-200 text-xs"
+                    side="top"
+                  >
+                    copy link to this page
+                  </HoverCardContent>
+                </HoverCard>
+                <HoverCard>
+                  <HoverCardTrigger>
+                    <Button
+                      className="w-10 h-10 bg-red-500 hover:bg-red-600"
+                      variant="destructive"
+                      onClick={() => router.push("/")}
+                    >
+                      <LogOut></LogOut>
+                    </Button>
+                  </HoverCardTrigger>
+                  <HoverCardContent
+                    className="py-1 px-2 w-auto bg-red-200 text-xs"
+                    side="top"
+                  >
+                    return to home
+                  </HoverCardContent>
+                </HoverCard>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+const SkeletonMirror = () => {
+  return (
+    <div className="relative min-h-screen">
+      <div className="flex flex-col items-center p-4 relative z-10">
+        <Card className="w-full max-w-6xl bg-inherit backdrop-blur-sm bg-opacity-0">
+          <CardHeader className="flex flex-row items-center justify-between">
+            <div className="flex items-center gap-2">
+              <Skeleton className="w-[6.3rem] h-[2.25rem] rounded bg-violet-300" />
+            </div>
+            <Skeleton className="w-20 h-6 rounded bg-green-300" />
+          </CardHeader>
+          <CardContent>
+            <Skeleton className="w-full min-h-[85vh] p-4 rounded-lg bg-neutral-200 border border-gray-300" />
+            <div className="mt-4 flex justify-end items-center">
+              <div className="flex gap-2">
+                <Skeleton className="w-10 h-10 rounded bg-blue-300" />
+                <Skeleton className="w-10 h-10 rounded bg-red-400" />
               </div>
             </div>
           </CardContent>
@@ -217,7 +286,7 @@ const Room = () => {
 };
 
 const RoomWrapper = () => (
-  <Suspense fallback={<div>Connecting to the room...</div>}>
+  <Suspense fallback={<SkeletonMirror />}>
     <Room />
   </Suspense>
 );
